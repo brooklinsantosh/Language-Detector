c8e79b01b5cfae5c5fa342a268650f53246fa0b7
brooklinsantosh <brooklinsantosh@gmail.com>
2023-01-13 21:45:15 +0530

    Get the absolute path with os.join()

--- src/const.py
+++ src/const.py
@@ -2,5 +2,5 @@ import os
 
 RANDOM_STATE = 42
 
-INPUT_FILE = os.path.join(os.getcwd(), "input\Language Detection.csv")
-MODEL_FILE = os.path.join(os.getcwd(), "output\LRModel.pckl")
+INPUT_FILE = os.path.join(os.getcwd(), "input", "Language Detection.csv")
+MODEL_FILE = os.path.join(os.getcwd(), "output","LRModel.pckl")
